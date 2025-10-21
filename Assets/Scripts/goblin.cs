e046578f4c16adade7f50b83b15138e0a78e9b27
QuangNhat10 <nhattqse181682@fpt.edu.vn>
2025-10-21 10:22:16 +0700

    Initial commit: Unity Top-Down Around Shooter project

---
+++ Assets/Scripts/goblin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goblin : MonoBehaviour
+{
+    public Animator animator;
+    private Vector3 lastPosition;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        lastPosition = transform.position;
+    }
+
+    void Update()
+    {
+        // Lấy vận tốc di chuyển hiện tại
+        float speed = (transform.position - lastPosition).magnitude / Time.deltaTime;
+
+        // Nếu đang di chuyển -> bật animation Run, nếu đứng yên -> Idle
+        animator.SetBool("isRun", speed > 0.05f);
+
+        lastPosition = transform.position;
+    }
+}
